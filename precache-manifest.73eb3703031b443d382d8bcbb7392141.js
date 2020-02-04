46249c8283ff5e849ea084cab9b43aa019b78dc3
orgad <orgad@svcollege.co.il>
2020-02-04 15:49:12 +0200

    Updates

---
+++ precache-manifest.73eb3703031b443d382d8bcbb7392141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8919f0e128f8ad8adc345af617603806",
+    "url": "/testShem/index.html"
+  },
+  {
+    "revision": "f4ea3d862b07d84af4d3",
+    "url": "/testShem/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ff74a2dfc1f3efb6fba5",
+    "url": "/testShem/static/js/2.889385df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testShem/static/js/2.889385df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4ea3d862b07d84af4d3",
+    "url": "/testShem/static/js/main.82002a7a.chunk.js"
+  },
+  {
+    "revision": "4271147a37ae38cf4896",
+    "url": "/testShem/static/js/runtime-main.7668ae5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testShem/static/media/logo.5d5d9eef.svg"
+  }
+]);
